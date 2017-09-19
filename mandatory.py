5620433a0ab573e6156e8758200328db7d055aa3
Bob-Xin <xingao1026@gmail.com>
2017-09-19 23:58:48 +0200

    this is mandatory assignment

---
+++ mandatory.py
@@ -0,0 +1,42 @@
+import nltk
+import urllib.request
+import ssl
+
+#retrieve the raw data from web and save it as txt
+ssl._create_default_https_context = ssl._create_unverified_context
+url = urllib.request.urlretrieve("https://raw.githubusercontent.com/AIHackers/DeepLearningStartUp/master/happiness_seg.txt","tt.txt")
+#open and read it in python
+fr = open("tt.txt","r",encoding="utf-8")
+text_test = fr.read()
+text_test_str = str(text_test)
+fr.close()
+#make the raw data as a list, splitted by "space"
+wordlist = text_test_str.split(" ")
+#to test what it looks like
+print (wordlist[:30])
+#make adjacent words combine together
+twowordslist = []
+for j in range(len(wordlist)-1):
+    twowordslist.append(wordlist[j]+wordlist[j+1])
+#have a check
+print (twowordslist[:30])
+#check the frequency of each combined word
+fdist = nltk.FreqDist(twowordslist)
+#make a dictionary which stores frequency as key, combined words as value
+dictionaryword = {}
+for word in fdist:
+    dictionaryword[fdist[word]] = word
+#check what it looks like
+print (dictionaryword)
+#create a list with frequency numbers in it and reverse sort the list from highest to lowest
+frequencylist = []
+for num in dictionaryword:
+    frequencylist.append(num)
+
+print (frequencylist)
+frequencylist.sort(reverse=True)
+print (frequencylist)
+#print out the combined word and its correspond frequency by line
+for i in frequencylist:
+    print (dictionaryword[i], "->", i)
+#manually check
